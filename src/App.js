211bd40b51814b22ca312be5a077828d869d2c20
LordAldi <aldianugra09@gmail.com>
2020-05-30 11:21:53 +0700

    add deletePersonHandler

--- src/App.js
+++ src/App.js
@@ -62,6 +62,11 @@ class App extends Component {
       }
     )
   }
+  deletePersonHandler= (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons})
+  }
   
   render(){
     const style={
@@ -76,8 +81,9 @@ class App extends Component {
     if (this.state.showPerson){
       persons = (
         <div>
-          {this.state.persons.map(person =>{
+          {this.state.persons.map((person, index) =>{
             return <Person 
+            click={() => this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
             />
