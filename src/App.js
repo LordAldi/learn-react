474c9cab81622c3e735d993249312c2418c6fb05
LordAldi <aldianugra09@gmail.com>
2020-05-30 13:59:56 +0700

    adding radium package, using it on button :hover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 // function App() {
 //   return (
@@ -81,7 +82,11 @@ class App extends Component {
       font:'inherit',
       border:'1px solid blue',
       padding:'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor:'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -101,6 +106,10 @@ class App extends Component {
         </div>
       )
       style.backgroundColor= 'red';
+      style[':hover'] = {
+        backgroundColor:'salmon',
+        color: 'black'
+      };
     }
 
     const classes = [];
@@ -128,4 +137,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
